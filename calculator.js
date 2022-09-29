abcaf8a93346a773a4fb97917193138fd87e9844
Oscar Elmahdy <opelm1@gmail.com>
2022-09-29 23:35:41 +0800

    Add functions for + - * /

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
-//
+// function evaluate() {}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (a === 0 || b === 0) {
+    return "U R DRUNK";
+  }
+  return a / b;
+}
